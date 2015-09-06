dca16036230fe08cce9527f9fbee2bc226c6eefa
Никола <mn080202@gmail.com>
2015-09-06 20:23:29 +0300

    exp function

---
+++ pifs.js
@@ -0,0 +1,34 @@
+function exp(p,ak){
+  var i,j,p1,pt,r,ntp=25,tp=[],tp1=0;
+  if(tp1==0){
+    tp1=1;
+    tp[0]=1.;
+  }
+  for(i=1;i<ntp;i++) tp[i]=2. * tp[i-1]
+    if (ak == 1.) return 0.;
+
+/*  Find the greatest power of two less than or equal to p. */
+
+  for (i = 0; i < ntp; i++) if (tp[i] > p) break;
+
+  pt = tp[i-1];
+  p1 = p;
+  r = 1.;
+
+/*  Perform binary exponentiation algorithm modulo ak. */
+
+  for (j = 1; j <= i; j++){
+    if (p1 >= pt){
+      r = 16. * r;
+      r = r - (parseInt(r / ak)) * ak;
+      p1 = p1 - pt;
+    }
+    pt = 0.5 * pt;
+    if (pt >= 1.){
+      r = r * r;
+      r = r - (parseInt(r / ak))* ak;
+    }
+  }
+
+  return r;
+}
